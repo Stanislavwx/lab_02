33c9d0891da8b4b445c683c4993dc76c9513d773
Stanislavwx <Stanislav.Chepara@lnu.edu.ua>
2025-04-16 18:00:18 +0300

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -5,19 +5,62 @@ double Vdob(double x1, double y1, double x2, double y2) {
     return x1 * y2 - y1 * x2;
 }
 
+
+// Геронова формула для площі трикутника за трьома сторонами
+double heronArea(double a, double b, double c) {
+    double s = (a + b + c) / 2.0; // півпериметр
+    return std::sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
 // Обчислення відстані між двома точками
 double distance(Point& p1, Point& p2) {
     return std::sqrt((p1.x - p2.x) * (p1.x - p2.x) +
         (p1.y - p2.y) * (p1.y - p2.y));
 }
 
+// Перевірка належності точки трикутнику за допомогою Герона
+bool isPointInTriangleHeron(Triangle& tri, Point& P) {
+    // Перевірка, чи трикутник вироджений
+    if (isDegenerate(tri)) {
+        // Якщо трикутник вироджений, перевірка на межі з використанням векторного добутку
+        return isPointOnTriangleEdge(tri, P); // Перевіряємо, чи точка на межі трикутника
+    }
+
+    double AB = distance(tri.A, tri.B);
+    double BC = distance(tri.B, tri.C);
+    double CA = distance(tri.C, tri.A);
+
+    double AP = distance(tri.A, P);
+    double BP = distance(tri.B, P);
+    double CP = distance(tri.C, P);
+
+    double S_main = heronArea(AB, BC, CA);
+    double S1 = heronArea(AP, BP, AB);
+    double S2 = heronArea(BP, CP, BC);
+    double S3 = heronArea(CP, AP, CA);
+
+    double S_sum = S1 + S2 + S3;
+
+    if (std::fabs(S_main - S_sum) < 1e-3) {
+        return true;  // Точка належить трикутнику
+    }
+
+    if (std::fabs(S_main - S_sum) < 1e-1) {
+       
+        return true;
+    }
+
+    return false;
+}
+
+
+
+
 // Перевірка, чи трикутник вироджений (площа = 0)
-
-
 bool isDegenerate(Triangle& tri) {
     double cross = Vdob(tri.B.x - tri.A.x, tri.B.y - tri.A.y,
         tri.C.x - tri.A.x, tri.C.y - tri.A.y);
-    return (std::fabs(cross) < 1e-9);
+    return (std::fabs(cross) < 1e-3);
 }
 
 // Перевірка, чи точка P лежить на відрізку AB
@@ -28,7 +71,7 @@ bool isPointOnEdge(Point& A, Point& B, Point& P) {
     double dAB = distance(A, B);
 
     // Перевірка, чи A+P+B = AB
-    return std::fabs((dAP + dPB) - dAB) < 1e-9;
+    return std::fabs((dAP + dPB) - dAB) < 1e-3;
 }
 
 // Перевірка, чи точка P лежить на межі трикутника
